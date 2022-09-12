7770d2754576b4e744e952bd79a716b7a00e7024
Kavi Mongia-Gasper <kavimongia-gasper@Kavis-MBP.fios-router.home>
2022-09-12 17:22:16 -0400

    my first commit

---
+++ hooks/useAuth.js
@@ -0,0 +1,32 @@
+import { createContext, useContext, useEffect, useState } from 'react';
+import { init, logIn as authLogIn} from '../lib/auth.js'
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState();
+    useEffect(() => {
+        init((user) => {
+            setUser(user)
+        })
+    }, [])
+
+    function logIn() {
+        authLogIn();
+    }
+
+    const contextValue = {
+        user,
+        logIn
+    }
+
+
+    return (
+        <AuthContext.Provider value={contextValue}>
+            { children }
+        </AuthContext.Provider>
+    )
+}
+
+export function useAuth() {
+    return useContext(AuthContext);
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
+import { AuthProvider } from '../hooks/useAuth';
+
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+  return (
+    <AuthProvider>
+    <Component {...pageProps} />
+      </AuthProvider>
+      );
+  } 
+
 
 export default MyApp
